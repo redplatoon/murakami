84326210876de5e492e50e7300084171ddc2ecd5
Nick Miano <nmiano84@gmail.com>
2016-03-05 14:09:28 -0500

    very basic http hello

--- project.clj
+++ project.clj
@@ -3,8 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-ring    "0.9.7"]
+            [lein-environ "1.0.2"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit            "2.1.18"]]
+                 [http-kit            "2.1.18"]
+                 [ring-server         "0.4.0"]
+                 [ring/ring-core      "1.4.0"]
+                 [compojure           "1.4.0"]
+                 [com.taoensso/sente  "1.8.1"]
+                 [environ             "1.0.2"]
+                 [com.taoensso/timbre "4.3.1"]
+                 [herolabs/apns       "0.5.0"]]
   :main ^:skip-aot murakami.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/murakami/core.clj
+++ src/murakami/core.clj
@@ -1,7 +1,25 @@
 (ns murakami.core
+  (:use org.httpkit.server)
   (:gen-class))
+
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    ;; graceful shutdown: wait 100ms for existing requests to be finished
+    ;; :timeout is optional, when no timeout, stop immediately
+    (@server :timeout 100)
+    (reset! server nil)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ;; The #' is useful when you want to hot-reload code
+  ;; You may want to take a look: https://github.com/clojure/tools.namespace
+  ;; and http://http-kit.org/migration.html#reload
+  (reset! server (run-server #'app {:port 8080})))
